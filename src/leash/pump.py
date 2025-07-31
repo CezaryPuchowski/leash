840cabe064c4df296851abbac2ac0d664b76c118
CezaryPuchowksi <puchowski.cezary@gmail.com>
2025-07-31 09:39:38 +0200

    pump ruff fixes and uncomment less power for pumps

--- src/leash/pump.py
+++ src/leash/pump.py
@@ -14,7 +14,7 @@ class Pump:
         self.sm = sm
         self.log = log
 
-    def get_pressure(self):
+    def get_pressure(self) -> int:
         """Read pressure from pump."""
         try:
             if self.index == "LEFT":
@@ -22,7 +22,6 @@ class Pump:
                 self.sm.send("M260 A112 B1 S1")
 
             elif self.index == "RIGHT":
-
                 #selects vac 2 through multiplexer
                 self.sm.send("M260 A112 B2 S1")
 
@@ -50,38 +49,12 @@ class Pump:
             if(result & (1 << 23)):
                 result = result - 2**24
 
-            return result
-
-        except Exception as e:
-            self.log.error(e)
+        except Exception:
+            self.log.exception()
             return False
 
-
-
-        # try:
-
-
-        #     #read addresses 0x06 0x07 and 0x08 for pressure reading
-        #     self.sm.send("M260 A109 B6 S1")
-        #     msb = re.search("data:(..)", self.sm.send("M261 A109 B1 S1"))
-
-        #     self.sm.send("M260 A109 B7 S1")
-        #     csb = re.search("data:(..)", self.sm.send("M261 A109 B1 S1"))
-
-        #     self.sm.send("M260 A109 B8 S1")
-        #     lsb = re.search("data:(..)", self.sm.send("M261 A109 B1 S1"))
-
-        #     val = msb.group(1)+csb.group(1)+lsb.group(1)
-
-        #     result = int(val, 16)
-
-        #     if(result & (1 << 23)):
-        #         result = result - 2**24
-
-        #     return result
-        # except Exception as e:
-        #     print(e)
-        #     return False
+        else:
+            return result
 
     def get_temperature(self)->bool:
         """Read temperature from pomp."""
@@ -92,14 +65,17 @@ class Pump:
             elif self.index == "RIGHT":
                 self.sm.send("M260 A112 B2 S1")
 
-            # Assuming sensor is an object or interface to communicate with the sensor
+            # Assuming sensor is an object or interface
+            # to communicate with the sensor
 
             # Read REG0x09 and REG0x0A
             self.sm.send("M260 A109 B9 S1")
-            reg0x09 = re.search("data:(..)", self.sm.send("M261 A109 B1 S1")).group(1)
+            reg0x09 = re.search("data:(..)",
+                                self.sm.send("M261 A109 B1 S1")).group(1)
 
             self.sm.send("M260 A109 B10 S1")
-            reg0x0a = re.search("data:(..)", self.sm.send("M261 A109 B1 S1")).group(1)
+            reg0x0a = re.search("data:(..)",
+                                self.sm.send("M261 A109 B1 S1")).group(1)
 
             # Calculate the temperature ADC value
             adc_value = int(reg0x09, base=16) * 256 + int(reg0x0a, base=16)
@@ -108,12 +84,11 @@ class Pump:
             if adc_value < 2**15:
                 # Temperature is positive
                 return adc_value / 256.0
-            else:
-                # Temperature is negative, apply the formula
-                return (adc_value - 2**16) / 256.0
+            # Temperature is negative, apply the formula
+            return (adc_value - 2**16) / 256.0
 
-        except Exception as e:
-            self.log.error(e)
+        except Exception:
+            self.log.exception()
             return False
 
     def off(self)->None:
@@ -133,13 +108,15 @@ class Pump:
             self.sm.send("M106")
             # turn on valve
             self.sm.send("M106 P1 S255")
-            # self.sm.send("G4 50")
-            # self.sm.send("M106 P1 S150")
+
+            self.sm.send("G4 50")
+            self.sm.send("M106 P1 S150")
 
         elif self.index == "RIGHT":
             #turn on pump
             self.sm.send("M106 P2 S255")
             #turn on valve
             self.sm.send("M106 P3 S255")
-            # self.sm.send("G4 50")
-            # self.sm.send("M106 P3 S150")
+
+            self.sm.send("G4 50")
+            self.sm.send("M106 P3 S150")
